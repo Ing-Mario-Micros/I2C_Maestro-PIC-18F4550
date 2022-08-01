38688d03d6e0c392a241c4b003d0d75ba7eb2eb9
Mario s <110110150+Ing-Mario-Micros@users.noreply.github.com>
2022-08-01 14:52:58 +0000

    Sistema de comunicación I2C Completo en liberia

--- .ccls-cache/@home@runner@Maestro-PIC-18F4550/I2C.h
+++ .ccls-cache/@home@runner@Maestro-PIC-18F4550/I2C.h
@@ -20,7 +20,9 @@ extern "C" {
 #endif
 
 #include<xc.h>
+char Vector_Datos[12];
 /*------------------------------ Funciones I2C ---------------------*/
+void Activar_I2C_Maestro(void);
 void Start(void);
 void Stop(void);
 void Rstart(void);
@@ -28,7 +30,15 @@ unsigned char LecturaDato(unsigned char,unsigned char);
 /*------------------------------ Funciones para DS ---------------------*/
 void Enviar_Dato_I2C(unsigned char);
 unsigned char Recibir_Dato_I2C(unsigned char);
+void Enviar_Dato_DSPIC(unsigned char,unsigned char,unsigned char);
+unsigned char Recibir_Dato_DSPIC(unsigned char,unsigned char);
 /*------------------------------ Funciones I2C ---------------------*/
+void Activar_I2C_Maestro(void){
+  SSPCON1=0b00101000;
+  SSPCON2=0b00000000;
+  SSPSTAT=0b11000000;
+  SSPADD=19;            //100kHz
+}
 void Start(void){    
   SEN=1;
   while(SEN==1);  
@@ -92,5 +102,101 @@ unsigned char Recibir_Dato_I2C(unsigned char Dato_Adicional){
     //LATE2=0;
     return SSPBUF;
 }
+void Enviar_Dato_DSPIC(unsigned char Direccion,unsigned char Registro, unsigned char Valor){
+  char Verificador_Temp;
+  /** Envio de Start **/
+  while(RB2 == 1);
+  __delay_ms(500);
+  Start();
+  LATC2=1;
+  LATC1=1;
+  /** Envio de Dirección **/
+  Enviar_Dato_I2C(Direccion & 0b11111110);
+  LATC2=0;
+  /** Envio de registro a trabajar **/
+  Enviar_Dato_I2C(Registro);
+  LATC2=1;
+  /** Envio de valor al Registro **/
+  Enviar_Dato_I2C(Valor);
+  LATC2=0;
+  /** Envio de Dirección modo Lectura **/
+  Rstart();
+  Enviar_Dato_I2C(Direccion | 0b00000001);
+  LATC2=1;
+  /** Resepción de Dato **/
+  Verificador_Temp=Recibir_Dato_I2C(0);
+  LATC2=0;
+  /** Envio de Dirección modo Escritura **/
+  Rstart();
+  Enviar_Dato_I2C(Direccion & 0b11111110);
+  LATC2=1;
+  /** Envio de valor al Registro **/
+  while(RB2 == 1);
+  __delay_ms(500);
+  if(Registro==Verificador_Temp){
+      Enviar_Dato_I2C(2);
+      LATC0=1;
+  }
+  else{
+      Enviar_Dato_I2C(1);
+      LATC0=0;
+  }
+  LATC2=0;
+  /** Envio de Stop **/
+  while(RB2 == 1);
+  __delay_ms(500);
+  Stop();
+  LATC2=1;
+  LATC1=0;
+}
+unsigned char Recibir_Dato_DSPIC(unsigned char Direccion , unsigned char Registro){
+    char Dato_Temp , Verificador_Temp;
+    /** Envio de Start **/
+  while(RB2 == 1);
+  __delay_ms(500);
+  Start();
+  LATC2=1;
+  LATC1=1;
+  
+  /** Envio de Dirección **/
+  Enviar_Dato_I2C(Direccion & 0b11111110);
+  LATC2=0;
+  
+  /** Envio de registro a trabajar **/
+  Enviar_Dato_I2C(Registro);
+  LATC2=1;
+  
+  /** Envio de Dirección modo Lectura **/
+  Rstart();
+  Enviar_Dato_I2C(Direccion | 0b00000001);
+  LATC2=0;
+  
+  /** Resepción de Dato **/
+  Dato_Temp=Recibir_Dato_I2C(1);
+  LATC2=1;
+  
+  /** Resepción de Dato **/
+  Verificador_Temp=Recibir_Dato_I2C(0);
+  LATC2=0;
+  
+  /** Envio de Stop **/
+  while(RB2 == 1);
+    __delay_ms(500);
+  Stop();
+  if(Registro==Verificador_Temp){
+      Vector_Datos[Registro]=Dato_Temp;
+      
+      LATC2=1;
+      LATC1=0;
+      return 1; 
+  }
+  else{
+      
+      LATC2=1;
+      LATC1=0;
+      return 0;
+  }
+}
+
 #endif	/* I2C_H */
 
--- main.c
+++ main.c
@@ -14,19 +14,8 @@
 #pragma config WDT=OFF
 #pragma config PBADEN=OFF
 
-
-
-/*------------------------------ Funciones I2C ---------------------*/
-
-
-/*------------------------------ Funciones para DS ---------------------*/
-unsigned char Recibir_Dato_DSPIC(unsigned char,unsigned char);
-void Enviar_Dato_DSPIC(unsigned char,unsigned char,unsigned char);
-
-
-
 /*------------------------------ variables del sistema -----------------*/
-char Vector_Datos[12] ;
+extern char Vector_Datos[12] ;
 
 void main(void){
   OSCCON=0b01110000;
@@ -36,10 +25,7 @@ void main(void){
   TRISB=0xFF;
   TRISC=0;
   LATC=0;
-  SSPCON1=0b00101000;
-  SSPCON2=0b00000000;
-  SSPSTAT=0b11000000;
-  SSPADD=19;
+  Activar_I2C_Maestro();
   while(1){
     __delay_ms(10);
       if(1== Recibir_Dato_DSPIC(0x10,0x1)){
@@ -56,109 +42,21 @@ void main(void){
           LATC0=0;
       }
       LATD=Vector_Datos[0x0];
-      Enviar_Dato_DSPIC(0x10,0x11,50);
+      Enviar_Dato_DSPIC(0x10,0x11,0);
     LATC2=1;
     __delay_ms(1000);
     LATC2=0;
     __delay_ms(1000);
   }  
 }
-/*---------------------------- Funciones I2C --------------------------------*/
-void Enviar_Dato_DSPIC(unsigned char Direccion,unsigned char Registro, unsigned char Valor){
-  char Verificador_Temp;
-  /** Envio de Start **/
-  while(RB2 == 1);
-  __delay_ms(500);
-  Start();
-  LATC2=1;
-  LATC1=1;
-  /** Envio de Dirección **/
-  Enviar_Dato_I2C(Direccion & 0b11111110);
-  LATC2=0;
-  /** Envio de registro a trabajar **/
-  Enviar_Dato_I2C(Registro);
-  LATC2=1;
-  /** Envio de valor al Registro **/
-  Enviar_Dato_I2C(Valor);
-  LATC2=0;
-  /** Envio de Dirección modo Lectura **/
-  Rstart();
-  Enviar_Dato_I2C(Direccion | 0b00000001);
-  LATC2=1;
-  /** Resepción de Dato **/
-  Verificador_Temp=Recibir_Dato_I2C(0);
-  LATC2=0;
-  /** Envio de Dirección modo Escritura **/
-  Rstart();
-  Enviar_Dato_I2C(Direccion & 0b11111110);
-  LATC2=1;
-  /** Envio de valor al Registro **/
-  while(RB2 == 1);
-  __delay_ms(500);
-  if(Registro==Verificador_Temp){
-      Enviar_Dato_I2C(2);
-      LATC0=1;
-  }
-  else{
-      Enviar_Dato_I2C(1);
-      LATC0=0;
-  }
-  LATC2=0;
-  /** Envio de Stop **/
-  while(RB2 == 1);
-  __delay_ms(500);
-  Stop();
-  LATC2=1;
-  LATC1=0;
-}
-unsigned char Recibir_Dato_DSPIC(unsigned char Direccion , unsigned char Registro){
-    char Dato_Temp , Verificador_Temp;
-    /** Envio de Start **/
-  while(RB2 == 1);
-  __delay_ms(500);
-  Start();
-  LATC2=1;
-  LATC1=1;
-  
-  /** Envio de Dirección **/
-  Enviar_Dato_I2C(Direccion & 0b11111110);
-  LATC2=0;
-  
-  /** Envio de registro a trabajar **/
-  Enviar_Dato_I2C(Registro);
-  LATC2=1;
-  
-  /** Envio de Dirección modo Lectura **/
-  Rstart();
-  Enviar_Dato_I2C(Direccion | 0b00000001);
-  LATC2=0;
-  
-  /** Resepción de Dato **/
-  Dato_Temp=Recibir_Dato_I2C(1);
-  LATC2=1;
-  
-  /** Resepción de Dato **/
-  Verificador_Temp=Recibir_Dato_I2C(0);
-  LATC2=0;
-  
-  /** Envio de Stop **/
-  while(RB2 == 1);
-    __delay_ms(500);
-  Stop();
-  if(Registro==Verificador_Temp){
-      Vector_Datos[Registro]=Dato_Temp;
-      
-      LATC2=1;
-      LATC1=0;
-      return 1; 
-  }
-  else{
-      
-      LATC2=1;
-      LATC1=0;
-      return 0;
-  }
-}
+
+
+
+
+
+
+
+
 
 
 
